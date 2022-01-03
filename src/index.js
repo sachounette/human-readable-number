6c2aebc3fd9a3ae2709ff072649bb22f783538bd
Mamina Papa <ada.norrsken@gmail.com>
2022-01-03 06:53:36 +0300

    add: add new changes to finish task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 module.exports = function toReadable (number) {
   const space = ' '
   const hundred = 'hundred'
+  const firstNumber = Math.floor(number / 100)
+  const additionalNumber = Math.floor(number % 100)
+  const secondNumber = Math.floor(additionalNumber / 10)
+  const thirdNumber = Math.floor(additionalNumber % 10)
+  const dozenNumFirst = Math.floor(number / 10)
+  const dozenNumSecond = Math.floor(number % 10)
   const nums1to19 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
   const dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
   if (number == 0) {
@@ -9,9 +15,24 @@ module.exports = function toReadable (number) {
     if (number > 0 && number < 20) {
       return nums1to19[number]
     } else if (number >= 20 && number < 100) {
-      const dozenNumFirst = Math.floor(number / 10)
-      const dozenNumSecond = Math.floor(number % 10)
-      return dozens[dozenNumFirst] + space + nums1to19[dozenNumSecond]
+      if (number % 10 == 0) {
+        return dozens[dozenNumFirst]
+      } else {
+        const finalDozen = dozens[dozenNumFirst] + space + nums1to19[dozenNumSecond]
+        return finalDozen
+      }
+    } else if (number >= 100 && number <= 999) {
+      if (additionalNumber == 0) {
+        return nums1to19[firstNumber] + space + hundred
+      } else if (thirdNumber == 0 && secondNumber == 1) {
+        return nums1to19[firstNumber] + space + hundred + space + nums1to19[additionalNumber]
+      } else if (thirdNumber == 0) {
+        return nums1to19[firstNumber] + space + hundred + space + dozens[secondNumber]
+      } else if (additionalNumber > 0 && additionalNumber < 20) {
+        return nums1to19[firstNumber] + space + hundred + space + nums1to19[additionalNumber]
+      } else {
+        return nums1to19[firstNumber] + space + hundred + space + dozens[secondNumber] + space + nums1to19[thirdNumber]
+      }
     }
   }
 }
